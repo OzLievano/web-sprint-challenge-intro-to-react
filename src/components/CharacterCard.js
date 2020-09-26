a9a1cd53f6f207116e5ee6bdf9b6465255f5fcec
Osvaldo Lievano <Osvaldoalievano@gmail.com>
2020-09-25 23:31:04 -0400

    created card content

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,8 +1,24 @@
 import React, { Component } from 'react';
+import {
+    Card, CardImg, CardText, CardBody,
+    CardTitle, CardSubtitle, Button,CardHeader
+  } from 'reactstrap';
+import styled from 'styled-components';
+
+const CardDiv = styled.div`
+  justify-content:center;
+`;
 
 const CharacterCard = (props) =>{
     return (
-        <div>CharacterCard</div>
+        <CardDiv>
+            <Card>
+                <CardHeader>{props.character.name}</CardHeader>
+                <CardBody>
+                    {`${props.character.name} is ${props.character.height} cm's tall, weighs ${props.character.mass} and has ${props.character.hair_color} hair and ${props.character.skin_color} skin`}
+                </CardBody>
+            </Card>
+        </CardDiv>
     )
 }
 
