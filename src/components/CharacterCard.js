094bb2802575905642b99f6a11637f1c10b983d5
Osvaldo Lievano <Osvaldoalievano@gmail.com>
2020-09-26 00:27:41 -0400

    created buttons with events

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -5,6 +5,10 @@ import {
   } from 'reactstrap';
 import styled from 'styled-components';
 
+const DivInfo = styled.div`
+  font-size:2em;
+  margin-bottom:1%;
+`;
 
 const CharacterCard = (props) =>{
     const [info,setInfo] = useState('');
@@ -14,7 +18,7 @@ const CharacterCard = (props) =>{
         setInfo(`${props.character.name} weighs ${props.character.mass} kg and is ${props.character.height} cm's tall with ${props.character.hair_color} hair and ${props.character.skin_color} skin`) 
     }
     const removeInfo = () =>{
-        let text = document.querySelector('.card p')
+        let text = document.querySelector('.card')
         setInfo('') 
     }
 
@@ -24,9 +28,11 @@ const CharacterCard = (props) =>{
             <Card>
                 <CardHeader>{props.character.name}</CardHeader>
                     <div className="card">
-                        <p>{info}
-                        <Button type="warning" onClick={showInfo}>Show Info</Button>
-                        <Button type="primary" onClick={removeInfo}>Hide Info</Button></p>
+                        <CardBody>
+                            <DivInfo>{info}</DivInfo>
+                            <Button color="warning" onClick={showInfo}>Show Info</Button>
+                            <Button color="primary" onClick={removeInfo}>Hide Info</Button>
+                        </CardBody>
                     </div>
             </Card>
         </div>
